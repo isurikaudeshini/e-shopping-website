c6e81b8653b84bf1aacff87f4b0f28cb056fc910
iamisu <isurika.ak@gmail.com>
2022-07-25 22:29:04 +0530

    Filtering paths

--- App-Server/routes/admin.js
+++ App-Server/routes/admin.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product',(req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form></input></form>');
+    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form></input></form>');
 });
 
 router.post('/product',(req, res, next) => {
