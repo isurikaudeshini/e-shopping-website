345d2b7ccc3fac94fdc431177d7fe8121109ec63
iamisu <isurika.ak@gmail.com>
2022-07-29 12:26:19 +0530

    Outputting Dynamic Content

--- App-Server/routes/admin.js
+++ App-Server/routes/admin.js
@@ -8,12 +8,15 @@ const router = express.Router();
 
 const products = [];
 
-router.get('/add-product',(req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
-router.post('/add-product',(req, res, next) => {
-    products.push({title: req.body.title});
-    res.redirect('/');
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
 exports.routes = router;
