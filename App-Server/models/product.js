caee6ab8f917f601d4a4066a477f4dac414e3143
iamisu <isurika.ak@gmail.com>
2022-08-02 21:54:55 +0530

    added forms.css & Storing data in Files via the model

--- App-Server/models/product.js
+++ App-Server/models/product.js
@@ -1,40 +1,46 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = class Product{
-    constructor(t) {
-        this.title = t;
-    }
+module.exports = class Product {
+  constructor(t) {
+    this.title = t;
+  }
 
-    save() {
-        const p = path.join(
-            path.dirname(require.main.filename),
-        'data', 
-        'products.json'
-        );
-        fs.readFile(p,(err, fileContent) => {
-            let products = [];
-            if(!err) {
-                products = JSON.parse(fileContent);
-            }
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            })
-        });
-    }
+  save() {
+    const p = path.join(
+      path.dirname(require.main.filename),
+      'data',
+      'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+        //! Add try catch block instead of if statement because, if statement did not detect the error 
+        try {
+          products = JSON.parse(fileContent);
+        } catch (err) {
+          products = [];
+        }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    });
+  }
 
-    static fetchAll() {
-        const p = path.join(
-            path.dirname(require.main.filename),
-        'data', 
-        'products.json'
-        );
-        fs.readFile(p, (err, fileContent) => {
-            if(err) {
-                return [];
-            }
-            return JSON.parse(fileContent);
-        })
-    }
-}
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(require.main.filename),
+      'data',
+      'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+       //! Add try catch block instead of if statement because, if statement did not detect the error 
+       try {
+        cb(JSON.parse(fileContent));
+      } catch (_err) {
+        cb([]);
+      }
+    });
+  }
+};
