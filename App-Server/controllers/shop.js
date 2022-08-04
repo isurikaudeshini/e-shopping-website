50423e57d137d585dc0cb5da029c1ba7c47634d7
iamisu <isurika.ak@gmail.com>
2022-08-04 23:18:14 +0530

    Rendering the product detail view

--- App-Server/controllers/shop.js
+++ App-Server/controllers/shop.js
@@ -1,52 +1,56 @@
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll((products) => {
     //new path defined to render
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'All Products',
-      path: '/products'       //path checking for in the navigation
+      path: '/products', //path checking for in the navigation
     });
   });
 };
 
 exports.getProduct = (req, res, next) => {
-  const prodId = req.params.productId;   //params access the name after the colon
- Product.findById(prodId, product =>{
-  console.log(product);
- });
-  res.redirect('/');
-}
+  const prodId = req.params.productId; //params access the name after the colon
+  Product.findById(prodId, (product) => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products',
+    });
+  });
+};
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll((products) => {
     // render index.ejs
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Shop',
-      path: '/'
+      path: '/',
     });
   });
-}
+};
 
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     path: '/cart',
-    pageTitle: 'Your Cart'
-  })
-}
+    pageTitle: 'Your Cart',
+  });
+};
 
 exports.getOrder = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
-    pageTitle: 'Your Orders'
-  })
-}
+    pageTitle: 'Your Orders',
+  });
+};
 
 exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout'), {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  }
-}
+  res.render('shop/checkout'),
+    {
+      path: '/checkout',
+      pageTitle: 'Checkout',
+    };
+};
