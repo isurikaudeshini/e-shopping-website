0edb48a080ceef17d81c6291b39fb5f144610035
iamisu <isurika.ak@gmail.com>
2022-08-08 23:00:38 +0530

    Editing the product data

--- App-Server/controllers/admin.js
+++ App-Server/controllers/admin.js
@@ -2,58 +2,70 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-    //new path defined to render
-    res.render('admin/edit-product', {
-      pageTitle: 'Add Product',
-      path: '/admin/add-product',
-      editing: false
-    });
-  };
-  
-  exports.postAddProduct = (req, res, next) => {
-    const title = req.body.title;
-    const imageUrl = req.body.imageUrl;
-    const price = req.body.price;
-    const description = req.body.description;
-    const product = new Product(title, imageUrl, price, description);
-    product.save();
-    res.redirect('/');
-  };
+  //new path defined to render
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false,
+  });
+};
 
-  exports.getEditProduct = (req, res, next) => {
-    //new path defined to render
-    const editMode = req.query.edit;
-    if(!editMode) {
-      console.log('im in edirmode')
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(null, title, imageUrl, price, description);
+  product.save();
+  res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  //new path defined to render
+  const editMode = req.query.edit;
+  if (!editMode) {
+    console.log('im in edirmode');
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      console.log(product);
       return res.redirect('/');
     }
-    const prodId = req.params.productId;
-    Product.findById(prodId, product => {
-      if (!product) {
-        console.log(product);
-        return res.redirect('/');
-      }
-      res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: editMode,
-        product: product 
-      });
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product,
     });
-  };
+  });
+};
 
-  exports.postEditProduct = (req, res, next) => {
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedPrice = req.body.price;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId, 
+    updatedTitle,  
+    updatedImageUrl,
+    updatedPrice, 
+    updatedDesc
+    );
+    updatedProduct.save();
+    res.redirect('/admin/products');
+};
 
-  };
-  
-  exports.getProducts = (req, res, next) => {
-    Product.fetchAll(products => {
-        // render products.ejs
-        res.render('admin/products', {
-          prods: products,
-          pageTitle: 'Admin Products',
-          path: '/admin/products'
-        });
-      });
-  };
-  
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    // render products.ejs
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products',
+    });
+  });
+};
--- App-Server/models/product.js
+++ App-Server/models/product.js
@@ -20,8 +20,9 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, price, description) {
+  constructor(id,title, imageUrl, price, description) {
     //initialize new product
+    this.id = id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.price = price;
@@ -30,12 +31,23 @@ module.exports = class Product {
 
   save() {
     //add a new property to entire product object
-    this.id = Math.random().toString();
     getProductsFromFile((products) => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
+      if(this.id) {
+        const existingProductIndex = products.findIndex(
+          prod => prod.id === this.id
+          );
+        const updatedProducts = [...products];
+        updatedProducts[existingProductIndex] = this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+          console.log(err);
+        });
+      } else {
+        this.id = Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err);
+        });
+      }
     });
   }
 
