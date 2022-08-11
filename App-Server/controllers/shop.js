eb229fc98399b0b11a54c0770397c24be72f3947
iamisu <isurika.ak@gmail.com>
2022-08-11 20:10:34 +0530

    Deleting Cart items and Fixing the bug

--- App-Server/controllers/shop.js
+++ App-Server/controllers/shop.js
@@ -39,7 +39,7 @@ exports.getCart = (req, res, next) => {
     Product.fetchAll(products => {
       const cartProducts = [];
       for(product of products) {
-        const cartProductData = cart.products.find(prod => prod.id === prod.id);
+        const cartProductData = cart.products.find(prod => prod.id === product.id);
         if(cartProductData) {
           cartProducts.push({productData: product, qty: cartProductData.qty});
         }
@@ -59,6 +59,14 @@ exports.postCart = (req, res, next) => {
     Cart.addProduct(prodId, product.price);
   });
   res.redirect('/cart');
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  });
 };
 
 exports.getOrder = (req, res, next) => {
