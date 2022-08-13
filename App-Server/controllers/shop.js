b52729517072484703fc2397d6788009d20addb0
iamisu <isurika.ak@gmail.com>
2022-08-13 22:22:12 +0530

    Retreiving data finding products

--- App-Server/controllers/shop.js
+++ App-Server/controllers/shop.js
@@ -2,33 +2,9 @@ const Cart = require('../models/cart');
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows, fieldData]) => {
-    res.render('shop/index', {
-      prods: rows,
-      pageTitle: 'Shop',
-      path: '/',
-    });
-  })
-};
-
-exports.getProduct = (req, res, next) => {
-  const prodId = req.params.productId; //params access the name after the colon
-  Product.findById(prodId)
-      .then(([product]) => {
-      res.render('shop/product-detail', {
-        product: product[0],
-        pageTitle: product.title,
-        path: '/products'
-      });
-  }).catch(err => console.log(err));
-};
-
-exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows, fieldData]) => {
-    res.render('shop/index', {
-      prods: rows,
+  Product.findAll().then(products => {
+    res.render('shop/product-list', {
+      prods: products,
       pageTitle: 'Shop',
       path: '/',
     });
@@ -36,7 +12,31 @@ exports.getIndex = (req, res, next) => {
   .catch( err => {
     console.error(err);
   });
+};
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId; //params access the name after the colon
+  Product.findById(prodId)
+      .then(([product]) => {
+      res.render('shop/product-detail', {
+        product: product,
+        pageTitle: product.title,
+        path: '/products'
+      });
+  }).catch(err => console.log(err));
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.findAll().then(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+    });
+  })
+  .catch( err => {
+    console.error(err);
+  });
 };
 
 exports.getCart = (req, res, next) => {
