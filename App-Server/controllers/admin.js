891d2c30078ae5f92e6e6dc85465fee30319e5be
iamisu <isurika.ak@gmail.com>
2022-08-18 19:38:06 +0530

    Using magic association methods

--- App-Server/controllers/admin.js
+++ App-Server/controllers/admin.js
@@ -15,18 +15,20 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description
-  }).then(result => {
-    console.log('Created Product');
-    res.redirect('/admin/products');
-  })
-  .catch(err => {
-    console.log(err);
-  });
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+    })
+    .then((result) => {
+      console.log('Created Product');
+      res.redirect('/admin/products');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -37,18 +39,18 @@ exports.getEditProduct = (req, res, next) => {
   }
   const prodId = req.params.productId;
   Product.findByPk(prodId)
-  .then((product) => {
-    if (!product) {
-      return res.redirect('/');
-    }  
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product,
-    });
-  })
-.catch(err => console.log(err)) 
+    .then((product) => {
+      if (!product) {
+        return res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product,
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -58,41 +60,42 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedDesc = req.body.description;
   Product.findByPk(prodId)
-  .then(product => {
-    product.title = updatedTitle;
-    product.price = updatedPrice;
-    product.description = updatedDesc;
-    product.imageUrl = updatedImageUrl;
-    return product.save();
-  })
-  .then(result => {
-    console.log('UPDATED PRODUCT');
-    res.redirect('/admin/products');
-  })
-  .catch(err => console.log(err))
-
+    .then((product) => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
+    })
+    .then((result) => {
+      console.log('UPDATED PRODUCT');
+      res.redirect('/admin/products');
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll().then((products) => {
-    // render products.ejs
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products',
-    });
-  })
-  .catch((err) => console.log(err));
+  Product.findAll()
+    .then((products) => {
+      // render products.ejs
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products',
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId).then((products) => {
-    products.destroy();
-  })
-  .then(result => {
-    console.log('DESTROYED PRODUCT');
-  })
-  .catch((err) => console.log(err));
+  Product.findByPk(prodId)
+    .then((products) => {
+      products.destroy();
+    })
+    .then((result) => {
+      console.log('DESTROYED PRODUCT');
+    })
+    .catch((err) => console.log(err));
   res.redirect('/admin/products');
 };
