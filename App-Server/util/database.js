462c6c28e418a1f63b6549882b5bd0d33ca0262e
iamisu <isurika.ak@gmail.com>
2022-08-23 14:01:36 +0530

    Installing the mongoDB driver

--- App-Server/util/database.js
+++ App-Server/util/database.js
@@ -1,8 +1,14 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'Appserver', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    'mongodb+srv://Isurika:SM7aEw8n5DDRxxl8@cluster0.3h6s7p1.mongodb.net/?retryWrites=true&w=majority'
+  )
+    .then((client) => {
+      console.log('Connected!');
+    })
+    .catch((err) => console.log(err));
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
