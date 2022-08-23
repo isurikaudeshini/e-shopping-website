eefe8426aac7ece7393b5e880cc5dd77611d282a
iamisu <isurika.ak@gmail.com>
2022-08-23 17:55:03 +0530

    Using the database connection

--- App-Server/models/product.js
+++ App-Server/models/product.js
@@ -1,5 +1,6 @@
 
 const getDb = require('../util/database').getDb;
+
 class Product {
   constructor(title, price, description, imageUrl) {
 this.title = title;
@@ -9,30 +10,14 @@ this.imageUrl = imageUrl;
   }
 
   save() {
-
+const db = getDb();
+db.collection('products')
+.insertOne(this)
+.then(result => {
+  console.log(result);
+})
+.catch(err => console.log(err));
   }
 }
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
-  }
-});
 
 module.exports = Product;
--- App-Server/util/database.js
+++ App-Server/util/database.js
@@ -5,7 +5,7 @@ let _db;
 
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    'mongodb+srv://Isurika:SM7aEw8n5DDRxxl8@cluster0.3h6s7p1.mongodb.net/?retryWrites=true&w=majority'
+   'mongodb+srv://Isurika:SM7aEw8n5DDRxxl8@cluster0.3h6s7p1.mongodb.net/?retryWrites=true&w=majority'
   )
     .then((client) => {
       console.log('Connected!');
