0ccfbb5d988e46304eda2d65fefd095cdeac47b6
iamisu <isurika.ak@gmail.com>
2022-08-23 22:47:15 +0530

    Fetching all products

--- App-Server/models/product.js
+++ App-Server/models/product.js
@@ -10,10 +10,24 @@ class Product {
 
   save() {
     const db = getDb();
-    return db.collection('products')
+    return db
+      .collection('products')
       .insertOne(this)
       .then((result) => {
         console.log(result);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection('products')
+      .find()
+      .toArray() //get all documents and put them in an arrray
+      .then((products) => {
+        console.log(products);
+        return products;
       })
       .catch((err) => console.log(err));
   }
