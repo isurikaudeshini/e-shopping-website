468d30257057713fba9724a44226d3a6de7d241e
iamisu <isurika.ak@gmail.com>
2022-08-26 08:47:40 +0530

    Working on cart items_orders

--- App-Server/models/user.js
+++ App-Server/models/user.js
@@ -4,9 +4,11 @@ const getDb = require('../util/database').getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart; //{items: []}-an object
+    this._id = id;
   }
 
   save() {}
@@ -15,12 +17,35 @@ class User {
     const db = getDb();
     return db.collection('users').insertOne(this);
   }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id;
+    // });
+    const updatedCart = {
+      /*updatedCart is an object, has items property array*/
+      items: [
+        {
+          ...product /*pullout all properties of this object_spread operator*/,
+          quantity: 1 /*in product the quantity property is overwrited*/,
+        },
+      ],
+    };
+    const db = getDb();
+    return db
+    .collection('users')
+    .updatedOne(
+      { _id: new ObjectId(this._id) },
+      { $set: { cart: updatedCart } } //overwritten by $set
+    );
+  }
+
   static findById(userId) {
     const db = getDb();
     return db
       .collection('users')
       .findOne({ _id: new ObjectId(userId) }) //findOne is an alternative for the find().next(),
-      .then(user => {
+      .then((user) => {
         console.log(user);
         return user;
       })
