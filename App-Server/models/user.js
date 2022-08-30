7070f507906b7b3e99a59e2e4488e7fb4aaffa80
iamisu <isurika.ak@gmail.com>
2022-08-30 18:12:32 +0530

    Working on the shopping cart

--- App-Server/models/user.js
+++ App-Server/models/user.js
@@ -3,23 +3,53 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    name:{
-        type:String,
-        required: true
-    },
-    email:{
-        type:String,
-        required: true
-    },
-    cart: {
-       items: [
-        {
-            productId:{type: Schema.Types.ObjectId,ref: 'Product', required: true}, // ref=> relation to product
-            quantity:{type: Number, required: true }
-        }
-       ]
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  cart: {
+    items: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        }, // ref=> relation to product
+        quantity: { type: Number, required: true },
+      },
+    ],
+  },
+});
+
+userSchema.methods.addToCart = function (product) {
+  let count = 0;
+  const cartProductIndex = this.cart.items.findIndex((cp) => {
+    console.log(cp.productId, product._id, count++);
+    return cp.productId.toString() == product._id.toString();
+  });
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity /*in product the quantity property is overwrited*/,
+    });
+  }
+  const updatedCart = {
+    /*updatedCart is an object, has items property array*/
+    items: updatedCartItems,
+  };
+  this.cart = updatedCart;
+  return this.save();
+};
 
 module.exports = mongoose.model('User', userSchema);
 
