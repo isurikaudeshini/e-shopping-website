8f7866b2092bda3a3ae9bf5e3d04da30cc2f1d07
iamisu <isurika.ak@gmail.com>
2022-09-02 11:23:32 +0530

    Using the session middleware

--- App-Server2/app.js
+++ App-Server2/app.js
@@ -15,21 +15,22 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin'); //registering the routes to reach them
 const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth'); 
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret: 'my secret', resave: false, saveUninitialized: false})
+app.use(
+  session({ secret: 'my secret', resave: false, saveUninitialized: false })
 );
 //session setup is a js object, secret is a long string, session will not be save for every request but some thing changed, ensuring no session will save unless changed
 
 app.use((req, res, next) => {
   User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
+    .then((user) => {
       req.user = user;
       next();
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -40,24 +41,25 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://Isurika:SM7aEw8n5DDRxxl8@cluster0.3h6s7p1.mongodb.net/shop?retryWrites=true&w=majority'
+    'mongodb+srv://Isurika:SM7aEw8n5DDRxxl8@cluster0.3h6s7p1.mongodb.net/shop?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true }
   )
-  .then(result => console.log('connected'))
-  .then(result => {
-    User.findOne().then(user => {
+  .then((result) => console.log('connected'))
+  .then((result) => {
+    User.findOne().then((user) => {
       if (!user) {
         const user = new User({
           name: 'Max',
           email: 'max@test.com',
           cart: {
-            items: []
-          }
+            items: [],
+          },
         });
         user.save();
       }
     });
     app.listen(3000);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
