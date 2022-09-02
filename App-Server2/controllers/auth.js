30c18150f0cd0368865c4ff1762ed38664b3ddc0
iamisu <isurika.ak@gmail.com>
2022-09-02 19:04:57 +0530

    Deleting a cookie

--- App-Server2/controllers/auth.js
+++ App-Server2/controllers/auth.js
@@ -1,15 +1,25 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-  // const isLoggedIn = req
-  //   .get('Cookie')
-  // .split('=')[1] === 'true';
-  console.log(req.session.isLoggedIn)
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
     isAuthenticated: false,
   });
 };
+
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  res.redirect('/');
+  User.findById('630cff6241b90202bac328f4').then((user) => {
+    req.session.isLoggedIn = true;
+    req.session.user = user;
+    res.redirect('/');
+  })
+  .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+     console.log(err);
+     res.redirect('/');
+  });
 };
