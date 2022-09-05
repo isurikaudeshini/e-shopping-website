dd9d7cfaf060ce4164864bae009f8c8de3148aa8
iamisu <isurika.ak@gmail.com>
2022-09-05 22:37:59 +0530

    implementing the token logic

--- App-Server3/controllers/auth.js
+++ App-Server3/controllers/auth.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 const bcrypt = require('bcryptjs'); //when hased can't recontruct or Decrypt
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
@@ -127,3 +129,34 @@ exports.getReset = (req, res, next) => {
   });
 };
 
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex'); //buffer will store hecadecimal val => convert to ASCII char
+    User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        req.flash('error', 'No account with that email found.');
+        return res.redirect('/reset');
+      }
+      user.resetToken = token;
+      user.resetTokenExpiration = Date.now() + 3600000;
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/');
+      transporter.sendMail({
+        to: req.body.email,
+        from: '<as95725@sci.sjp.ac.lk>',
+        subject: 'Password Reset',
+        html: `
+        <p>You requested password reset</p>
+        <p>Click this <a href="http://localhost:3000/reset/${token}" >link</a> to set a new password</p>`,
+      });
+    })
+    .catch((err) => console.log(err));
+  });
+};
