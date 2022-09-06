100e854e20ae169dd3585b9f7c40e6d2eb080c4d
iamisu <isurika.ak@gmail.com>
2022-09-06 16:06:08 +0530

    Adding conditional CSS classes

--- App-Server3/controllers/auth.js
+++ App-Server3/controllers/auth.js
@@ -44,7 +44,8 @@ exports.getSignup = (req, res, next) => {
       email: '',
       password: '',
       confirmPassword: ''
-    }
+    },
+    validationErrors: []
   });
 };
 
@@ -101,6 +102,7 @@ exports.postSignup = (req, res, next) => {
         password: password,
         confirmPassword: req.body.confirmPassword,
       },
+      validationErrors: errors.array()
     });
   }
   bcrypt
