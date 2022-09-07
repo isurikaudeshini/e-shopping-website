a1983f3de29cbecab46529edcc8a32d0aba53fac
iamisu <isurika.ak@gmail.com>
2022-09-08 00:37:51 +0530

    Using Error handlinf middleware correctly

--- App-Server3/app.js
+++ App-Server3/app.js
@@ -43,6 +43,13 @@ app.use(flash()); //function
 app.use(csrfProtection);
 
 app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
+  // throw new Error('Sync Dummy')
   if (!req.session.user) {
     return next();
   }
@@ -55,14 +62,8 @@ app.use((req, res, next) => {
       next();
     })
     .catch((err) => {
-      throw new Error(err);
+      next(new Error(err));
     });
-});
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
 });
 
 app.use('/admin', adminRoutes);
@@ -74,7 +75,12 @@ app.get('/500', errorController.get500);
 app.use(errorController.get404);
 
 app.use((error, req, res, next) => {
-  res.redirect('/500')
+  // res.redirect('/500')
+  res.status(500).render('500', {
+    pageTitle: 'Error',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
 })
 
 mongoose
