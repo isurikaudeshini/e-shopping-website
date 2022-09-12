97cb171aa4a6a9c5642678c70b70bf6c9ded2cda
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-09-12 11:00:00 +0530

    Handling multipart form data

--- App-Server3/routes/admin.js
+++ App-Server3/routes/admin.js
@@ -21,7 +21,7 @@ router.post('/add-product',
     body('title').isString()
     .isLength({ min: 3})
     .trim(),
-    body('imageUrl').isURL(),
+    body('imageUrl'),
     body('price').isFloat(),
     body('description')
     .isLength({min: 5, max:400})
