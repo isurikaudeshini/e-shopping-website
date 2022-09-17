60dfdaf3ca8d238b14596df37d1b232ff3785a68
iamisu <isurika.ak@gmail.com>
2022-09-17 22:16:55 +0530

    Serving images statically

--- App-Server3/app.js
+++ App-Server3/app.js
@@ -55,7 +55,10 @@ const csrfProtection = csrf();
 app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 ); //buffer is turned into binary , and store in file
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images',express.static(path.join(__dirname, 'images')));
+
 app.use(
   session({
     secret: 'my secret',
