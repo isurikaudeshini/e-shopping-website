55e971b6caa843a0eff380c8a503fbb7086818d2
iamisu <isurika.ak@gmail.com>
2022-09-19 01:16:20 +0530

    Sending & Handling background requests

--- App-Server3/public/js/admin.js
+++ App-Server3/public/js/admin.js
@@ -1,7 +1,19 @@
 //This will not run on the server, but on on the client server side. So in the browser.
 
 const deleteProduct = (btn) => {
-const prodId = btn.parentNode.querySelector('[name=productId]').value;
-const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+  const prodId = btn.parentNode.querySelector('[name=productId]').value;
+  const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
 
+  fetch('/admin/product/' + prodId, {
+    method: 'DELETE',
+    headers: {
+      'csrf-token': csrf,
+    },
+  })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
