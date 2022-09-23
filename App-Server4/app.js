8f97b5f5ea986babd119368264ff7481c3bce118
iamisu <isurika.ak@gmail.com>
2022-09-24 01:19:21 +0530

    Static images and error handling

--- App-Server4/app.js
+++ App-Server4/app.js
@@ -1,7 +1,9 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
@@ -9,6 +11,7 @@ const app = express();
 
 // app.use(bodyParser.urlencoded(());
 app.use(bodyParser.json()); //application/json
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -18,6 +21,13 @@ app.use((req, res, next) => {
 })
 
 app.use('/feed',feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({message});
+});
 
 mongoose.connect(
     'mongodb+srv://Isurika:SM7aEw8n5DDRxxl8@cluster0.3h6s7p1.mongodb.net/shop?retryWrites=true&w=majority'
