ae9ab3be50e43de69d6d8c17ebfcb6ade2dba258
iamisu <isurika.ak@gmail.com>
2022-09-24 00:29:04 +0530

    Setting up a Post Model

--- App-Server4/app.js
+++ App-Server4/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
@@ -17,4 +19,8 @@ app.use((req, res, next) => {
 
 app.use('/feed',feedRoutes);
 
-app.listen(8080);
+mongoose.connect(
+    'mongodb+srv://Isurika:SM7aEw8n5DDRxxl8@cluster0.3h6s7p1.mongodb.net/shop?retryWrites=true&w=majority'
+  ).then(result => {
+    app.listen(8080);
+  }).catch(err => console.log(err));
