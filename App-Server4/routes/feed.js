a0bcf691a50b59403b6a3531bca5f3aae828c085
iamisu <isurika.ak@gmail.com>
2022-09-24 21:42:50 +0530

    Updating post

--- App-Server4/routes/feed.js
+++ App-Server4/routes/feed.js
@@ -20,4 +20,10 @@ router.post(
 
 router.get('/post/:postId', feedController.getPost);
 
+router.put('/post/:postId', [
+  body('title').trim().isLength({ min: 5 }),
+  body('content').trim().isLength({ min: 5 }),
+], feedController.updatePost); 
+//put => throgh asynvhronous requests triggered by post by JS you can use, has req body , params
+
 module.exports = router;
