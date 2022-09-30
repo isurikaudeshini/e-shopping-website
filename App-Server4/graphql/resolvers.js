7f2f8806f9ff11f468cfd694da96f16f83f735ea
iamisu <isurika.ak@gmail.com>
2022-09-30 20:46:05 +0530

    Adding Input Validation

--- App-Server4/graphql/resolvers.js
+++ App-Server4/graphql/resolvers.js
@@ -1,10 +1,25 @@
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
 const User = require('../models/user');
 
 module.exports = {
   createUser: async function ({ userInput }, req) {
     // const email = args.userInput.email;
+    const errors = [];
+    if (!validator.isEmail(userInput.email)) {
+      errors.push({ message: 'Invalid email, Please enter a valid email' });
+    }
+    if (
+      validator.isEmpty(userInput.password) ||
+      !validator.isLength(userInput.password, { min: 5 })
+    ) {
+      errors.push({ message: 'Password is too short!' });
+    }
+    if (errors.length > 0) {
+        const error = new Error('Invalid input');
+        throw error;
+    }
     const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
       const error = new Error('User already exists!');
@@ -14,10 +29,10 @@ module.exports = {
     const user = new User({
       email: userInput.email,
       name: userInput.name,
-      password: hashedPw
+      password: hashedPw,
     });
     const createdUser = await user.save();
-    return {...createdUser._doc, _id: createdUser._id.toString()}
+    return { ...createdUser._doc, _id: createdUser._id.toString() };
   },
 };
 //need a method for every query defined in Schema
