25befee8dcbfab8f41888b1c4167be1298d45eb6
iamisu <isurika.ak@gmail.com>
2022-10-01 11:26:50 +0530

    Adding a createPost mutation

--- App-Server4/graphql/resolvers.js
+++ App-Server4/graphql/resolvers.js
@@ -3,6 +3,7 @@ const validator = require('validator');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+const Post = require('../models/post');
 
 module.exports = {
   createUser: async function ({ userInput }, req) {
@@ -18,10 +19,10 @@ module.exports = {
       errors.push({ message: 'Password is too short!' });
     }
     if (errors.length > 0) {
-        const error = new Error('Invalid input');
-        error.data = errors;
-        error.code = 422;
-        throw error;
+      const error = new Error('Invalid input');
+      error.data = errors;
+      error.code = 422;
+      throw error;
     }
     const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
@@ -37,8 +38,8 @@ module.exports = {
     const createdUser = await user.save();
     return { ...createdUser._doc, _id: createdUser._id.toString() };
   },
-  login: async function({ email, password }) {
-    const user = await User.findOne({ email: email});
+  login: async function ({ email, password }) {
+    const user = await User.findOne({ email: email });
     if (!user) {
       const error = new Error('User not found');
       error.code = 401;
@@ -50,14 +51,49 @@ module.exports = {
       error.code = 401;
       throw error;
     }
-    const token = jwt.sign({
-      userId: user._id.toString(),
-      email: user.email
-        },
-        'somesupersecretsecret',
-        { expiresIn: '1h' }
-        );
-        return { token: token, userId: user._id.toString() };
-  }
+    const token = jwt.sign(
+      {
+        userId: user._id.toString(),
+        email: user.email,
+      },
+      'somesupersecretsecret',
+      { expiresIn: '1h' }
+    );
+    return { token: token, userId: user._id.toString() };
+  },
+  createPost: async function ({ postInput }, req) {
+    const errors = [];
+    if (
+      validator.isEmpty(postInput.title) ||
+      !validator.isLength(postInput.title, { min: 5 })
+    ) {
+      errors.push({ message: 'Title is Invalid ' });
+    }
+    if (
+      validator.isEmpty(postInput.content) ||
+      !validator.isLength(postInput.content, { min: 5 })
+    ) {
+      errors.push({ message: 'Content is Invalid ' });
+    }
+    if (errors.length > 0) {
+      const error = new Error('Invalid input');
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+    const post = new Post({
+      title: postInput.title,
+      content: postInput.content,
+      imageUrl: postInput.imageUrl,
+    });
+    const createdPost = await post.save();
+    //Add post to user;'s posts
+    return {
+      ...createdPost._doc,
+      _id: createdPost._id.toString(),
+      createdAt: createdPost.createdAt.toISOString(),
+      UpdatedAt: createdPost.updatedAt.toISOString(),
+    };
+  },
 };
 //need a method for every query defined in Schema
