0c1892b154f0e03f5270fc072e6c9ed49e31fceb
iamisu <isurika.ak@gmail.com>
2022-10-08 21:39:03 +0530

    Tesitng Auth-Middleware

---
+++ App-Server-6 testing/test/start.js
@@ -0,0 +1,14 @@
+// const expect = require('chai').expect
+// //mocha => running our tests
+// //chai=> defining success conditions
+// it('should add numbers correctly', function() {
+//     const num1 = 2;
+//     const num2 = 3;
+//     expect(num1 + num2).to.equal(5);
+// })
+
+// it('should not give result of 6', function() {
+//     const num1 = 3;
+//     const num2 = 3;
+//     expect(num1 + num2).not.to.equal(6);
+// })
