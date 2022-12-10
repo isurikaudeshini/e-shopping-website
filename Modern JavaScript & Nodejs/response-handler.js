69c38a8189c158ed653d0ca5de21a2de35e841ec
iamisu <isurika.ak@gmail.com>
2022-12-11 00:39:10 +0530

    Node core Modules & Promises

--- Modern JavaScript & Nodejs/response-handler.js
+++ Modern JavaScript & Nodejs/response-handler.js
@@ -1,5 +1,5 @@
 // const fs = require('fs');
-import fs  from 'fs';
+import fs  from 'fs/promises';
 import path, { dirname} from 'path';
 
 import { fileURLToPath } from 'url';
@@ -9,10 +9,13 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
   // Define a function to export to app.js
 export const resHandler =  (req, res, next) => {
-    // fs.readFile('my-page.html', 'utf8', (err, data) => {
-    //   res.send(data);
-    // });
-    res.sendFile(path.join(__dirname, 'my-page.html'));
+    fs.readFile('my-page.html', 'utf8').then((data)=> {
+      res.send(data);
+    })
+     .catch((err) => {
+      console.log(err);
+    });
+    // res.sendFile(path.join(__dirname, 'my-page.html'));
   };
 
 // exporting
