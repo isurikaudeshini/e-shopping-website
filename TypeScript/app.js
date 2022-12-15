aa73c40bfc22bb5179ab4bed4bb1208b16138ac0
iamisu <isurika.ak@gmail.com>
2022-12-15 14:14:29 +0530

    Working with Union Types

--- TypeScript/app.js
+++ TypeScript/app.js
@@ -1,13 +1,21 @@
-"use strict";
-const num1Element = document.getElementById('num1'); //Typecast to HTMLInputElement 
-const num2Element = document.getElementById('num2');
-const buttonElement = document.querySelector('button');
+var num1Element = document.getElementById('num1'); //Typecast to HTMLInputElement 
+var num2Element = document.getElementById('num2');
+var buttonElement = document.querySelector('button');
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    }
+    else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + ' ' + num2;
+    }
+    else
+        return +num1 + +num2;
 }
-buttonElement.addEventListener('click', () => {
-    const num1 = num1Element.value;
-    const num2 = num2Element.value;
-    const result = add(+num1, +num2); //value is a string , converting to a number
+buttonElement.addEventListener('click', function () {
+    var num1 = num1Element.value;
+    var num2 = num2Element.value;
+    var result = add(+num1, +num2); //value is a string , converting to a number
     console.log(result);
+    var stringresult = add(num1, num2);
+    console.log(stringresult);
 });
--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -2,13 +2,19 @@ const num1Element = document.getElementById('num1') as HTMLInputElement; //Typec
 const num2Element = document.getElementById('num2') as HTMLInputElement;
 const buttonElement = document.querySelector('button')!;
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
+function add(num1: number | string, num2: number | string) {
+   if (typeof num1 === 'number' && typeof num2 === 'number') {
+      return num1 + num2;
+   } else if (typeof num1 === 'string' && typeof num2 === 'string') { 
+  return num1 + ' ' + num2;
+} else return +num1 + +num2;
 }
 
 buttonElement.addEventListener('click',() => {
    const num1 = num1Element.value;
    const num2 = num2Element.value;
-   const result = add(+num1, +num2); //value is a string , converting to a number
+   const result = add(num1, num2); //value is a string , converting to a number
    console.log(result);
+   const stringresult = add(num1, num2);
+   console.log(stringresult);
 })
